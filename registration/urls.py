9c715a9c0b7aff048138c7799969cd687fc60621
sovandara1607 <srith3@paragoniu.edu.kh>
2026-04-22 14:13:10 +0700

    Add home route to registration URLs for improved navigation

--- registration/urls.py
+++ registration/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.student_register, name='home'),
     path('student/', views.student_register, name='student_register'),
     path('course/', views.course_register, name='course_register'),
     path('report/', views.course_report, name='course_report'),
